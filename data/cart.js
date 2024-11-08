0e8a96d16c589f83529de4a9b29a9d0005d4f1ac
Ilma <makarevic85@gmail.com>
2024-11-09 00:20:57 +0100

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,21 @@
 export const cart = []; //now variable cart can be used outside of cart.js 
+
+
+export function addToCart(productId){
+    let matchingItem;
+
+    cart.forEach((cartItem) => {
+        if(productId === cartItem.productId){
+            matchingItem = cartItem;
+        }
+    });
+
+    if(matchingItem) {
+        matchingItem.quantity += 1;
+    } else {
+        cart.push({
+            productId: productId,
+            quantity: 1
+        });
+    }
+}
